8d6ec451a644957359e02ef6d36fb58a6e413c4a
Dani <danikav1989@gmail.com>
2021-03-20 18:28:35 +0000

    classes created

---
+++ models/class.py
@@ -0,0 +1,8 @@
+class Gymclass:
+
+    def __init__(self, name, date, time, details, id=None):
+        self.name = name
+        self.date = date
+        self.time = time
+        self.details = details
+        self.id = id
